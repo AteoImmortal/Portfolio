9e1100e8742cfa9769485d6a44dbed4afd0b3019
AteoImmortal <105009513+AteoImmortal@users.noreply.github.com>
2022-08-27 10:41:59 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 const navIcon = document.querySelector('.nav-icon');
 const nav__mobile = document.querySelector('.nav__mobile');
+const overlay = document.querySelector('#overlay');
 
 
 navIcon.addEventListener('click', function () {
     this.classList.toggle('nav-icon--active');
     nav__mobile.classList.toggle('nav__mobile--active');
+    overlay.classList.toggle('active')
+    
 })
